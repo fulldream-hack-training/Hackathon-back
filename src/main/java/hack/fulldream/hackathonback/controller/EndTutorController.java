cafc5cfa602d4c66965635591b9007ad1888fb77
Kirisaki_VK <kirisaki.vk@gmail.com>
2024-01-27 00:38:16 +0300

    fix: signup access

--- src/main/java/hack/fulldream/hackathonback/controller/EndTutorController.java
+++ src/main/java/hack/fulldream/hackathonback/controller/EndTutorController.java
@@ -3,12 +3,16 @@ package hack.fulldream.hackathonback.controller;
 import hack.fulldream.hackathonback.models.EndTutor;
 import hack.fulldream.hackathonback.service.EndTutorService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,8 +25,15 @@ public class EndTutorController {
         return endTutorService.findByEmail(authentication.getName());
     }
 
-    @GetMapping("/tutors")
+    @GetMapping
     public List<EndTutor> getAll() {
         return endTutorService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EndTutor> getById(@PathVariable UUID id) {
+        Optional<EndTutor> tutor = endTutorService.findById(id);
+
+        return tutor.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
