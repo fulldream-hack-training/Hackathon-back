8ccd12925edd16e314122c66fba7a6ab9aa0ff03
Tendry24 <hei.tendry.2@gmail.com>
2024-01-26 23:17:31 +0300

    feat: /api/kid

--- src/main/java/hack/fulldream/hackathonback/repository/BalanceRepository.java
+++ src/main/java/hack/fulldream/hackathonback/repository/BalanceRepository.java
@@ -1,2 +1,12 @@
-package hack.fulldream.hackathonback.repository;public interface BalanceRepository {
+package hack.fulldream.hackathonback.repository;
+
+import hack.fulldream.hackathonback.models.Balance;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface BalanceRepository extends JpaRepository<Balance, UUID> {
+
 }
