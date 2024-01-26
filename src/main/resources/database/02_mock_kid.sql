38ec4c5a76b8cef9c6226e7111a26739651b072b
MiiJack <f.rama723@gmail.com>
2024-01-26 22:08:29 +0300

    feat: Add mock data for the database

---
+++ src/main/resources/database/02_mock_kid.sql
@@ -0,0 +1,6 @@
+INSERT INTO kid (id,id_tutor,first_name,last_name,points)
+VALUES('0c87305c-5fc0-40fe-9a6f-01a9276d0502', 'edd1c771-9a0a-4904-aa0c-fe45a1b96920',
+       'Reina', 'Jo', 17);
+INSERT INTO kid (id,id_tutor,first_name,last_name,points)
+VALUES('d5e9f2fe-119d-486a-b7fb-ac13af8b6b57', 'f6e17cf1-3e2d-413e-be19-9a1efc574aca',
+       'Razanamalao', 'Samoina', 12.5);
---
+++ src/main/resources/database/06_mock_savings.sql
@@ -0,0 +1,4 @@
+INSERT INTO savings(id,rate,value,id_kid,start_date)
+VALUES('2ddd7773-309d-426d-ba95-f017e97d89f4', 1.5, 0, '0c87305c-5fc0-40fe-9a6f-01a9276d0502', '2024-01-26');
+INSERT INTO savings(id,rate,value,id_kid,start_date)
+VALUES('3b055a36-63ec-4392-b92a-1a875ee88e58', 3.5, 1000, 'd5e9f2fe-119d-486a-b7fb-ac13af8b6b57', '2024-01-25');
